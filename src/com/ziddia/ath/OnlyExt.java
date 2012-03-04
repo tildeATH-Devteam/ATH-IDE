eddd6bab072226dac1098485e9f033eae5da5bba
Ziddia <ziddia@me.com>
2012-03-04 15:54:26 -0500

    Added auto-imports and documentations.

---
+++ src/com/ziddia/ath/OnlyExt.java
@@ -0,0 +1,17 @@
+package com.ziddia.ath;
+
+import java.io.*;
+
+public class OnlyExt implements FilenameFilter { 
+	
+	String ext; 
+	public OnlyExt(String ext) 
+	{ 
+		this.ext = "." + ext; 
+	} 
+	public boolean accept(File dir, String name) 
+	{ 
+		return name.endsWith(ext); 
+	}
+	
+}
--- src/com/ziddia/ath/Viewer.java
+++ src/com/ziddia/ath/Viewer.java
@@ -5,6 +5,9 @@ import java.awt.event.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
@@ -52,15 +55,51 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
         menu2.add(makeMenuItem("~ATH"));
         menu2.add(makeMenuItem("^CAKE"));
         //menu2.add(makeMenuItem("&VIL"));
+        menu2.add(makeMenuItem("DIS*"));
         Menu menu3 = new Menu("Color");
         menu3.add(makeMenuItem("Red"));
         menu3.add(makeMenuItem("Blue"));
         menu3.add(makeMenuItem("Green"));
         menu3.add(makeMenuItem("Yellow"));
+        Menu menu4 = new Menu("Packages");
+        Menu menu5 = new Menu("Positions");
+        menu5.add(makeMenuItem("Universe"));
+        menu5.add(makeMenuItem("Dimension"));
+        menu5.add(makeMenuItem("World"));
+        menu5.add(makeMenuItem("Session"));
+        menu5.add(makeMenuItem("User"));
+        menu5.add(makeMenuItem("Location"));
+        Menu menu6 = new Menu("Objects");
+        menu6.add(makeMenuItem("Object"));
+        menu6.add(makeMenuItem("Solid"));
+        menu6.add(makeMenuItem("Liquid"));
+        menu6.add(makeMenuItem("Gas"));
+        menu6.add(makeMenuItem("Plasma"));
+        menu4.add(menu5);
+        menu4.add(menu6);
+        Menu menu7 = new Menu("Declarations");
+        Menu menu8 = new Menu("Positions");
+        menu8.add(makeMenuItem("Open Universe"));
+        menu8.add(makeMenuItem("Open Dimension"));
+        menu8.add(makeMenuItem("Open World"));
+        menu8.add(makeMenuItem("Open Session"));
+        menu8.add(makeMenuItem("Open User"));
+        menu8.add(makeMenuItem("Open Location"));
+        Menu menu9 = new Menu("Objects");
+        menu9.add(makeMenuItem("Open Object"));
+        menu9.add(makeMenuItem("Open Solid"));
+        menu9.add(makeMenuItem("Open Liquid"));
+        menu9.add(makeMenuItem("Open Gas"));
+        menu9.add(makeMenuItem("Open Plasma"));
+        menu7.add(menu8);
+        menu7.add(menu9);
         MenuBar menuBar = new MenuBar();
         menuBar.add(menu);
         menuBar.add(menu1);
         menuBar.add(menu2);
+        menuBar.add(menu3);
+        menuBar.add(menu4);
+        menuBar.add(menu7);
         setMenuBar(menuBar);
         pack();
 	}
@@ -68,6 +107,13 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
 	private MenuItem makeMenuItem(String name)
     {
         MenuItem m = new MenuItem(name);
+        m.addActionListener(this);
+        return m;
+    }
+	
+	private MenuItem makeMenuItem(String name, char shortcut)
+    {
+        MenuItem m = new MenuItem(name, new MenuShortcut(shortcut));
         m.addActionListener(this);
         return m;
     }
@@ -121,8 +167,8 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
         else if (command.equals("^CAKE"))
         {
         	textArea.setText("");
-        	textArea.setBackground(Color.ORANGE);
-    		textArea.setForeground(Color.RED);
+        	textArea.setBackground(Color.GREEN);
+    		textArea.setForeground(Color.ORANGE);
     		textArea.setFont(new Font("Courier", Font.BOLD, 12));
         }
         else if (command.equals("&VIL"))
@@ -132,6 +178,60 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
     		textArea.setForeground(Color.BLACK);
     		textArea.setFont(new Font("Courier", Font.BOLD, 12));
         }
+        else if (command.equals("DIS*"))
+        {
+        	textArea.setText("");
+        	textArea.setBackground(Color.ORANGE);
+    		textArea.setForeground(Color.CYAN);
+    		textArea.setFont(new Font("Courier", Font.BOLD, 12));
+        }
+        else if (command.equals("Red") || command.equals("Blue") || command.equals("Green") || command.equals("Yellow"))
+        	JOptionPane.showMessageDialog(textArea, "Colors are currently broken.", "~ATHINFORMER", JOptionPane.WARNING_MESSAGE);
+        else if (command.equals("Universe"))
+        	textArea.setText("import Universe u;\n" + textArea.getText());
+        else if (command.equals("Dimension"))
+        	textArea.setText("import Dimension d;\n" + textArea.getText());
+        else if (command.equals("World"))
+        	textArea.setText("import World w;\n" + textArea.getText());
+        else if (command.equals("Session"))
+        	textArea.setText("import Session s;\n" + textArea.getText());
+        else if (command.equals("User"))
+        	textArea.setText("import User u1;\n" + textArea.getText());
+        else if (command.equals("Location"))
+        	textArea.setText("import Location l;\n" + textArea.getText());
+        else if (command.equals("Solid"))
+        	textArea.setText("import Solid s1;\n" + textArea.getText());
+        else if (command.equals("Liquid"))
+        	textArea.setText("import Liquid l1;\n" + textArea.getText());
+        else if (command.equals("Gas"))
+        	textArea.setText("import Gas g;\n" + textArea.getText());
+        else if (command.equals("Plasma"))
+        	textArea.setText("import Plasma p;\n" + textArea.getText());
+        else if (command.equals("Object"))
+        	textArea.setText("import Object o;\n" + textArea.getText());
+        else if (command.equals("Open Universe"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/Universe.html");
+        else if (command.equals("Open Dimension"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/Dimension.html");
+        else if (command.equals("Open World"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/World.html");
+        else if (command.equals("Open Session"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/Session.html");
+        else if (command.equals("Open User"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/User.html");
+        else if (command.equals("Open Location"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/locations/Location.html");
+        else if (command.equals("Open Solid"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/objects/Solid.html");
+        else if (command.equals("Open Liquid"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/objects/Liquid.html");
+        else if (command.equals("Open Gas"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/objects/Gas.html");
+        else if (command.equals("Open Plasma"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/objects/Plasma.html");
+        else if (command.equals("Open Object"))
+        	openUrl("http://zidmc.com/alpha/ath/declarations/objects/Object.html");
+        
 	}
 	
 	private void buildFile()
@@ -142,6 +242,7 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
             // Show the dialog box to the user.
             FileDialog fd = new FileDialog(this, "Build File", FileDialog.SAVE);
             fd.setDirectory(System.getProperty("user.dir"));
+            fd.setFilenameFilter(new OnlyExt("ATH"));
             fd.show();
 
             //Get the path and file name.
@@ -198,6 +299,7 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
         //Show the Open File dialog box to the user.
         FileDialog fd = new FileDialog(this, "Open File", FileDialog.LOAD);
         fd.setDirectory(System.getProperty("user.dir"));
+        fd.setFilenameFilter(new OnlyExt("~ATH"));
         fd.show();
 
         //Get the file path.
@@ -232,6 +334,7 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
             // Show the dialog box to the user.
             FileDialog fd = new FileDialog(this, "Save File", FileDialog.SAVE);
             fd.setDirectory(System.getProperty("user.dir"));
+            fd.setFilenameFilter(new OnlyExt("~ATH"));
             fd.show();
 
             //Get the path and file name.
@@ -273,6 +376,27 @@ public class Viewer extends JFrame implements ActionListener, KeyListener {
 	public void keyTyped(KeyEvent arg0) 
 	{
 		
+	}
+	
+	public void openUrl(String arg)
+	{
+		if(!Desktop.isDesktopSupported()) 
+		{
+            System.err.println("Desktop is not supported!");
+        }
+		Desktop desktop = Desktop.getDesktop();
+		if(!desktop.isSupported(Desktop.Action.BROWSE)) 
+		{
+            System.err.println("Desktop doesn't support the browse action!");
+        }
+        try {
+        	URI uri = new URI(arg);
+			desktop.browse(uri);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
